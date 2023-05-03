d514a602a5b60b021ac4ec3c5cbf0fd1c59a50a8
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-05-03 14:09:56 +0800

    Add files via upload

--- day_data.py
+++ day_data.py
@@ -1107,6 +1107,12 @@ fig_new.savefig(name)
 plt.close()
 
 
+import telegram
+bot = telegram.Bot(token='6219784883:AAE3YXlXvxNArWJu-0qKpKlhm4KaTSHcqpw')
+bot.sendDocument(chat_id='-840309715', document=open('/root/chain_every_day/比特币链上数据一览图.png', 'rb'))
+
+
+
 
 
 
