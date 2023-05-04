41ce7c72e0c5bfb0a39055330521a0a1a7cb5582
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-05-04 18:50:53 +0800

    Add files via upload

--- day_data.py
+++ day_data.py
@@ -1104,16 +1104,16 @@ ax14.tick_params(labelsize=10)
 ax14.legend(loc='upper left', fontsize=5)
 ax14.set_ylabel("7MA aSOPR",fontsize=10)
 
-fig_new.text(0.5, 0.9, '0xCarson出品，lian xi+V：shengxiaruchu009', transform=ax.transAxes,
-        fontsize=50, color='gray', alpha=0.5,
+fig_new.text(0.5, 0.95, '0xCarson出品，lian xi+V：shengxiaruchu009', transform=ax.transAxes,
+        fontsize=150, color='gray', alpha=0.5,
+        ha='center', va='center', rotation='30',fontproperties = prop)
+
+fig_new.text(0.5, 0.85, '0xCarson出品，lian xi+V：shengxiaruchu009', transform=ax.transAxes,
+        fontsize=150, color='gray', alpha=0.5,
         ha='center', va='center', rotation='30',fontproperties = prop)
 
 fig_new.text(0.5, 0.75, '0xCarson出品，lian xi+V：shengxiaruchu009', transform=ax.transAxes,
-        fontsize=50, color='gray', alpha=0.5,
-        ha='center', va='center', rotation='30',fontproperties = prop)
-
-fig_new.text(0.5, 0.6, '0xCarson出品，lian xi+V：shengxiaruchu009', transform=ax.transAxes,
-        fontsize=50, color='gray', alpha=0.5,
+        fontsize=150, color='gray', alpha=0.5,
         ha='center', va='center', rotation='30',fontproperties = prop)
 
 name = str(title_asset_pool) + '比特币链上数据一览图' + '.png'
